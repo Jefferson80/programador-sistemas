af3638c81c3b0ae6532724b70b88bdaacf3cd114
Jefferson80 <44246534+Jefferson80@users.noreply.github.com>
2018-10-18 21:11:06 -0300

    Add files via upload

---
+++ aula07/controller02/src/ExemploFlutuante/ExemploFlutuante.java
@@ -0,0 +1,18 @@
+package ExemploFlutuante;
+
+public class ExemploFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario, aumento, novoSalario;
+		
+		salario = 2000.00;
+		aumento = 0.15;
+		novoSalario = salario + (aumento * salario);
+		System.out.println("Novo Salário R$ "  + novoSalario);
+		
+	}
+	
+	
+	
+}
---
+++ aula07/controller02/src/ExemploInteiro/ExemploInteiro.java
@@ -0,0 +1,21 @@
+package ExemploInteiro;
+
+public class ExemploInteiro {
+
+	public static void main(String[] args) {
+		
+		int numero1, numero2;
+		float numero3, soma;
+		
+		
+		numero1 = 12;
+		numero2 = 9;
+		numero3 = 4;
+		soma = numero1 + numero2 + numero3;
+		System.out.println("Valor da Soma: " + soma);
+		
+		
+	}
+	
+	
+}
