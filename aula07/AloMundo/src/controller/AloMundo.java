797d017c4434b353dd680cff525a35ac9e7cebdc
Jefferson80 <44246534+Jefferson80@users.noreply.github.com>
2018-10-18 21:13:06 -0300

    Add files via upload

---
+++ aula07/AloMundo/src/controller/AloMundo.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class AloMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Alô Mundo");
+		
+		System.out.println();
+		System.out.println();
+		
+
+	}
+	
+}
